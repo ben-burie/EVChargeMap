a22cf8c1bfc308756fbc15d822d98500306946ac
ben-burie <benburie22@gmail.com>
2025-11-26 15:13:08 -0600

    Saving trip & tripstop info to database

--- tripManager.py
+++ tripManager.py
@@ -11,11 +11,48 @@ connection = mysql.connector.connect(
     database=os.getenv('DB_NAME')
 )
 
-def saveTrip(trip_data): # Change this function to write trip data to SQL database
+cursor = connection.cursor()
 
-    print("Trip car: ", trip_data.get('car'))
-    #print("Start location: ", trip_data.get())
-    print("Start: ", trip_data.get('start_location').get('location'))
-    print("End: ", trip_data.get('end_location').get('location'))
+def saveTrip(trip_data):
+    car_id = getCarID(trip_data.get('car'))
+    print("Car ID: ", car_id)
 
-    print("Trip data saved (mock):", trip_data)
+    result = cursor.callproc('CreateTrip', [
+        1, # Mock user ID
+        car_id,
+        "test_trip", # Mock trip name
+        trip_data.get('start_location').get('location'), #start location
+        trip_data.get('end_location').get('location'), #end location
+        0 #out parameter placeholder
+    ])
+
+    trip_ID = result[-1]
+    saveTripStop(trip_ID, trip_data.get('stops'))
+    
+    connection.commit()
+
+def saveTripStop(trip_id, stop_data):
+    for stop in stop_data:
+        cursor.callproc('AddTripStop', [
+            trip_id,
+            stop.get('order'), #stop_seq
+            stop.get('id') #station id
+        ])
+
+        connection.commit()
+
+def getCarID(car_name):
+    cursor.callproc('LoadAllCars')
+    for result in cursor.stored_results():
+        rows = result.fetchall()
+        for row in rows:
+            if car_name == row[0] + " " + row[1]:
+                car_brand = row[0]
+                car_model = row[1]
+                cursor.callproc('GetCarID', [car_brand, car_model])
+                car_id = None
+                for result in cursor.stored_results():
+                    rows = result.fetchall()
+                    if rows:
+                        car_id = rows[0][0]
+    return car_id
